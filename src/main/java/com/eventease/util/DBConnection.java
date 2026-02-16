cc883d7ab0d06529c729959c9e41a4404a9ffd21
NarmadhaR20 <narmadhashivum72506@gmail.com>
2026-02-16 21:22:00 +0530

    fixed railway mysql connection

--- src/main/java/com/eventease/util/DBConnection.java
+++ src/main/java/com/eventease/util/DBConnection.java
@@ -6,17 +6,20 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL =
-        "jdbc:mysql://mysql.railway.internal:3306/railway?useSSL=false";
-
+    private static final String HOST = "mysql.railway.internal";
+    private static final String PORT = "3306";
+    private static final String DATABASE = "railway";
     private static final String USER = "root";
-
     private static final String PASSWORD = "YGNERyiIrwWYfYFhXJzqWEMBDEWiCtZM";
+
+    private static final String URL =
+        "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE +
+        "?useSSL=true&requireSSL=true&verifyServerCertificate=false&allowPublicKeyRetrieval=true";
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("MySQL JDBC Driver loaded successfully");
+            System.out.println("Driver loaded");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -24,8 +27,11 @@ public class DBConnection {
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected successfully!");
+            return conn;
         } catch (SQLException e) {
+            System.out.println("Connection FAILED");
             e.printStackTrace();
             return null;
         }
